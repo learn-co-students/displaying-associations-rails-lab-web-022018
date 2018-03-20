0cd12f91a0f20ed16f441318078d798b2816d17f
vsparrow <khan@vsparrow.com>
2018-03-20 18:44:23 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  # Artist#
+  def song_count
+    self.songs.count
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  # Song#
+  def artist_name
+    self.artist.name if self.artist
+  end
 end
