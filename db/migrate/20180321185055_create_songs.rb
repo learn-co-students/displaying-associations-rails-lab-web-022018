631f199b788effb9c25bef41227552daa7c48ae6
nkalkstein <nkalkstein@gmail.com>
2018-03-21 14:51:51 -0400

    Done.

--- db/migrate/20180321185055_create_songs.rb
+++ db/migrate/20180321185055_create_songs.rb
@@ -2,8 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-
-      t.timestamps null: false
+      t.integer :artist_id
     end
   end
 end
